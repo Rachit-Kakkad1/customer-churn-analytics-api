520cabbab5fab49b8ba731fa4d3017ecee9a17df
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-05-13 14:51:57 +0530

    feat: implement authentication field validations

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,6 +13,8 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Name is required'],
       trim: true,
+      minlength: [2, 'Name must be at least 2 characters'],
+      maxlength: [50, 'Name cannot exceed 50 characters'],
     },
     email: {
       type: String,
@@ -20,10 +22,13 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address'],
     },
     password: {
       type: String,
       required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+      select: false, // Don't return password by default
     },
     role: {
       type: String,
