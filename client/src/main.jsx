110c7dcafcda3c573ce28bfb32efb7629672a063
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-06-17 23:39:42 +0530

    feat: implement dashboard notification system

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeProvider.jsx'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Toaster theme="dark" position="top-right" richColors />
     </ThemeProvider>
   </StrictMode>,
 )
